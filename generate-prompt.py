45e442bfe563c000a49450eec02db73dabd748a3
Davi Campos <121815039+caamps@users.noreply.github.com>
2024-07-17 11:11:04 -0300

    Update generate-prompt.py

--- generate-prompt.py
+++ generate-prompt.py
@@ -1,23 +1,22 @@
 import json
-import boto3
 import random
 
 def lambda_handler(event, context):
-    
     question = event['node']['inputs'][0]['value']
     paths = 5
-    
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('questions-table')
-    items = table.scan()['Items']
-    
-    prompts = ['' for i in range(paths)]
+
+    with open('questions.json', 'r') as f:
+        items = json.load(f)
+
+    prompts = ['' for _ in range(paths)]
     for i in range(paths):
-        random_numbers = random.sample(range(0, 7), 3)
+        random_numbers = random.sample(range(len(items)), 3)
         for r in random_numbers:
             prompts[i] += items[r]['question'] + '\n'
-    
+
     for i in range(paths):
         prompts[i] += 'Pergunta: ' + question + '\nResposta: '
-    
+
+    print(prompts[0])
+
     return prompts
